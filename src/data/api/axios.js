5aa624f2c4a0572b6ccedf7aaedbc5d548522562
Diyorbek Asatullayev <122860422+asatullayev-diyorbek@users.noreply.github.com>
2026-03-13 00:50:36 +0500

    Update API base URL in axios configuration

--- src/data/api/axios.js
+++ src/data/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://erpbackend.pythonanywhere.com/api/v1",
+  baseURL: "https://apiprotech.asatullayev.uz/api/v1",
 });
 
 /* =========================
